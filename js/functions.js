1664c33e291b3a55562e27df5a6aa84d918920c9
anita <epp.anita@icloud.com>
2018-05-26 09:32:00 -0500

    js Fix
    Fixed smooth-scroll

--- js/functions.js
+++ js/functions.js
@@ -2,18 +2,22 @@
 
 jQuery(document).ready(function($){
 	
+		"use strict";
+	
 		$('.smooth-scroll').on('click', 'a', function(e) {
    			
 			e.preventDefault();
-		   
-			var hash = this.hash;
 			
-			$('html, body').animate({
-			   scrollTop: $(this.hash).offset().top-105
-			 }, 500, function(){
-		
-			   window.location.hash = hash;
-			 });
+			var hash = this.hash;
+		   
+			var target_offset = $(this.hash).offset() ? $(this.hash).offset().top : 0;
+			     
+			var customoffset = 105;
+			
+			$('html, body').animate({scrollTop:target_offset - customoffset}, 500);
+
+			
+			window.location.hash = hash;
 		
 		});
 
